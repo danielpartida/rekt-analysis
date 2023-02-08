cde70930eefb71468dae6853ba8644aaacea1d51
daniel <partidapd@gmail.com>
2023-02-08 11:57:41 +0100

    Add dynamic query to get rekt descriptions

--- src/queries.py
+++ src/queries.py
@@ -28,22 +28,32 @@ query {
 }
 """
 
-# TODO: Make this query dynamic and change the pageSize to 50
-description_query = """
-query {
-  rekts(
-    pageNumber:1
-    pageSize:10
-    orderBy: {
-      fundsLost: desc
+
+def get_rekt_description_query(page_number: int = 1, page_size: int = 50) -> str:
+    """
+    Dynamically fetches description rekts based on page_number and page_size.
+    If no parameters are given, page_number starts at 1, and page_size is 50
+    :param page_number: int
+    :param page_size: int
+    :return: str dynamic query
+    """
+    description_query = """
+    query {
+      rekts(
+        pageNumber:%s
+        pageSize:%s
+        orderBy: {
+          fundsLost: desc
+        }
+      ) {
+        id
+        projectName
+        description
+      }
     }
-  ) {
-    id
-    projectName
-    description
-  }
-}
-"""
+    """ % (page_number, page_size)
+
+    return description_query
 
 
 def get_rekt_query(page_number: int = 1, page_size: int = 50) -> str:
