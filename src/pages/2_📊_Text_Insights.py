0648c38ffef7dd66732fe0e7bc91a8e4b1a42249
daniel <partidapd@gmail.com>
2023-02-08 12:22:43 +0100

    Create topic modelling task

--- src/pages/2_📊_Text_Insights.py
+++ src/pages/2_📊_Text_Insights.py
@@ -1,23 +1,13 @@
-import pandas as pd
-from queries import description_query
-from main import execute_gql_query, get_graphql_client
-from transformers import pipeline
-
-task = "question-answeringn"
-model = "distilbert-base-cased-distilled-squad"
-question_answerer = pipeline(task, model=model)
+from main import get_all_rekt_summaries, get_graphql_client
+from bertopic import BERTopic
 
 gql_client = get_graphql_client()
-description_response = execute_gql_query(client=gql_client, query=description_query)
-df = pd.DataFrame(description_response['rekts'])
 
-conversation = '''Jeff: Can I train a 🤗 Transformers model on Amazon SageMaker? 
-Philipp: Sure you can use the new Hugging Face Deep Learning Container. 
-Jeff: ok.
-Jeff: and how can I get started? 
-Jeff: where can I find documentation? 
-Philipp: ok, ok you can find everything here. https://huggingface.co/blog/the-partnership-amazon-sagemaker-and-hugging-face                                           
-'''
+df = df = get_all_rekt_summaries(client=gql_client, limit=175)
+docs = df.to_list()
 
-response_summary = question_answerer(conversation)
-summary = response_summary[0]['summary_text']
+topic_model = BERTopic(verbose=True, n_gram_range=(1, 3), min_topic_size=5)
+topics, probs = topic_model.fit_transform(docs)
+
+fig = topic_model.visualize_topics()
+fig.show()
